3d688a8885f7705ea378b7b00634405ee627b668
sleaqse <luluboypascual@gmail.com>
2025-03-08 16:40:24 +0800

    Update selcad.lua

--- selcad.lua
+++ selcad.lua
@@ -75,7 +75,7 @@ local Tp = true
 ASection:NewToggle("Auto OrbTP", "Teleports you to orbs automatically (portal relic)", function(State)
     Tp = State
 task.spawn(function()
-    while Tp and wait(0.1) do
+    while Tp and do
 	local MyFol
         local CombatFolder = workspace:FindFirstChild("CombatFolder")
         if CombatFolder ~= nil and CombatFolder:FindFirstChild(Player.Name) then
